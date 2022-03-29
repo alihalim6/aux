130ea9c165a5ee58323515105c664ae76a426055
Ali Halim <alihalim6@gmail.com>
2022-03-29 12:51:21 -0400

    this is dumb

--- api/routes/index.js
+++ api/routes/index.js
@@ -7,5 +7,6 @@ router.use('/newAndRecommended', require('../newAndRecommended'));
 router.use('/playback', require('../playback'));
 router.use('/detail', require('../detail'));
 router.use('/artist', require('../artist'));
+//!TODO: add new paths to vercel config
 
 module.exports = router;
--- api/v1.js
+++ api/v1.js
@@ -1,13 +1,7 @@
 const bodyParser = require('body-parser');
 const app = require('express')();
-import newAndRecommended from './newAndRecommended';
 
 app.use(bodyParser.json())
-//app.use('/api', require('./routes'));
-
-app.use('/api/newAndRecommended', newAndRecommended);
-/* app.use('/playback', require('./playback'));
-app.use('/detail', require('./detail'));
-app.use('/artist', require('./artist')); */
+app.use('/api', require('./routes'));
 
 module.exports = app;

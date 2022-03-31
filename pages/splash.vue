d61c8972e7da153ccbfa29d3e21e64c8e2567c81
Ali Halim <alihalim6@gmail.com>
2022-03-31 19:29:19 -0400

    Update splash.vue

--- pages/splash.vue
+++ pages/splash.vue
@@ -7,7 +7,7 @@
           <v-btn class="login-button" @click="loginClicked()">Login with Spotify</v-btn>
 
           <div class="under-construction">
-            <div class="tbd-message">*This application is still in development.  Apps in development that use Spotify's API have to use a beta.  
+            <div class="tbd-message">*This web application is still in development.  Apps in development that use Spotify's API have to use a beta.  
               If you're interested in getting added to the list and seeing the app at its current stage, let's connect! Message me on LinkedIn and I'll get you added right away.  
               You'll then be able to load the app and see what Aux is all about!</div>
             <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="large" data-theme="light" data-type="HORIZONTAL" data-vanity="alihalim6" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://www.linkedin.com/in/alihalim6?trk=profile-badge">Ali Halim</a></div>

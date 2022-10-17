c28b570c323d2c19ec0b9d65c91f434612d708f1
Ali Halim <alihalim6@gmail.com>
2022-10-17 16:00:38 -0400

    previous version of socket index file to see if that is why build failed

--- io/index.js
+++ io/index.js
@@ -1,5 +1,3 @@
-//https://github.com/nuxt/nuxt.js/blob/2ec62617ced873fef97c73a6d7aa1271911ccfd5/examples/with-sockets/io/index.js
-
 import http from 'http';
 import socketIO from 'socket.io';
 
@@ -13,30 +11,10 @@ export default function(){
     // close this server on 'close' event
     this.nuxt.hook('close', () => new Promise(server.close));
 
-    io.on('connection', socket => {
-      socket.on('userLive', data => {
-        if(data){//refresh at same/similar time seems to cause empty data
-          socket.broadcast.emit('handleLiveUser', data.userProfile);
-        }
-      });
-
-      socket.on('activityAdded', activity => {
-        socket.broadcast.emit('handleActivity', activity);
-      });
-
-      socket.on('activityReactionAdded', reaction => {
-        socket.broadcast.emit('handleActivityReactionAdded', reaction);
-      });
-
-      socket.on('disconnect', reason => {
-        console.log(reason);
-        
-        if(reason == 'io server disconnect' || reason == 'io client disconnect'){
-          console.log('attempting reconnection...');
-          socket.connect();
-        }
-
-        io.emit('handleUserDisconnect');
+    // Add socket.io events
+    io.on('connection', (socket) => {
+      socket.on('goingLive', function(id){
+        socket.broadcast.emit('liveSession', id);
       });
     });
   });

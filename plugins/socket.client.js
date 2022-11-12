e1ac71a76b2d5bad2b9a91cd8070de5d8c3179f6
Ali Halim <alihalim6@gmail.com>
2022-11-12 14:00:01 -0500

    comment out socket imports

--- plugins/socket.client.js
+++ plugins/socket.client.js
@@ -1,6 +1,6 @@
 //https://github.com/nuxt/nuxt.js/blob/2ec62617ced873fef97c73a6d7aa1271911ccfd5/examples/with-sockets/plugins/socket.io.js
 
-import io from 'socket.io-client';
-const socket = io(process.env.WS_URL);
+//import io from 'socket.io-client';
+//const socket = io(process.env.WS_URL);
 
-export default socket;
+export default null;

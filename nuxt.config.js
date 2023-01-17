a73163fb1f946beff6d3f07b98911d038406df8a
Ali Halim <alihalim6@gmail.com>
2023-01-17 10:24:27 -0500

    use vercel env variable for WS URL

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,3 @@
-import {BASE_URL} from "./utils/constants";
-
 export default {  
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
@@ -47,7 +45,7 @@ export default {
   ],
 
   env: {
-    WS_URL: BASE_URL
+    WS_URL: process.env.WS_URL || 'http://localhost:3000'
   },
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
